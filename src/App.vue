746e51d17d0e0727aa386659c6be4e54f2b33bdc
tiantian <yanttde@outlook.com>
2020-12-07 20:21:39 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,17 @@
  * @Author: 严田田
  * @Date: 2020-12-07 16:05:23
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-12-07 16:48:12
+ * @LastEditTime: 2020-12-07 20:19:06
 -->
 <template>
   <div id="app">
     <h1>显示页面</h1>
+    <i class="iconfont icondianzan"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
  * @Author: 严田田
  * @Date: 2020-12-07 16:05:23
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-12-07 17:01:10
+ * @LastEditTime: 2020-12-07 20:17:40
  */
 import Vue from 'vue'
 import App from './App.vue'
@@ -12,6 +12,14 @@ import router from './router'
 import store from './store'
 // 加载全局样式
 import './styles/index.less'
+
+// 加载vant核心组件
+import Vant from 'vant'
+// 加载vant文件夹
+import 'vant/lib/index.css'
+
+// 注册vant
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
